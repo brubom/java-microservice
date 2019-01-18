b7d038276a2a5f8af5e1577439a6dafceb44cb7a
Bruno Moreira <brubom@gmail.com>
2019-01-18 14:18:53 +0000

    First version, not working yet

--- src/main/java/com/brubom/api/Application.java
+++ src/main/java/com/brubom/api/Application.java
@@ -1,4 +1,16 @@
 package com.brubom.api;
 
-public class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class Application extends SpringBootServletInitializer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(Application.class);
+  }
 }
